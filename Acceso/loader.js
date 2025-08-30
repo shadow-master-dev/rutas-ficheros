554a31ba85607c4681bf1f0b491053c40831556d
shadow-master-dev <master.blogger.design@gmail.com>
2025-08-30 08:35:40 -0500

    Create loader.js

---
+++ Acceso/loader.js
@@ -0,0 +1,16 @@
+    <script>
+      const loader = document.getElementById("loader");
+      const triggers = document.querySelectorAll(".menu-trigger");
+
+      triggers.forEach(link => {
+        link.addEventListener("click", e => {
+          e.preventDefault(); // evitar salto inmediato
+          loader.classList.add("show");
+
+          // Simulación de espera antes de ir al link real
+          setTimeout(() => {
+            window.location.href = link.getAttribute("href");
+          }, 1800); // 1.8s con animación elegante
+        });
+      });
+    </script>
