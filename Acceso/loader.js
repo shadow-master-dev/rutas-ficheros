5b53ba0c6c63767f0facc9669a4ea824801ae52c
shadow-master-dev <master.blogger.design@gmail.com>
2025-08-30 14:05:03 -0500

    Update loader.js

--- Acceso/loader.js
+++ Acceso/loader.js
@@ -1,29 +1,27 @@
-<script>
-  // ===== FUNCIONES =====
-  function mostrarLoader() {
-    document.getElementById("page-loader").classList.add("show");
-  }
+// ===== FUNCIONES =====
+function mostrarLoader() {
+  document.getElementById("page-loader").classList.add("show");
+}
 
-  function ocultarLoader() {
-    document.getElementById("page-loader").classList.remove("show");
-  }
+function ocultarLoader() {
+  document.getElementById("page-loader").classList.remove("show");
+}
 
-  // ===== EVENTOS =====
-  document.querySelectorAll(".nav-link").forEach(link => {
-    link.addEventListener("click", function(e) {
-      const url = this.getAttribute("href");
-      if(url && url !== "#"){
-        e.preventDefault();
-        mostrarLoader(); // mostrar loader tipo slider
-        setTimeout(() => {
-          window.location.href = url;
-        }, 1500);
-      }
-    });
+// ===== EVENTOS =====
+document.querySelectorAll(".nav-link").forEach(link => {
+  link.addEventListener("click", function(e) {
+    const url = this.getAttribute("href");
+    if(url && url !== "#"){
+      e.preventDefault();
+      mostrarLoader();
+      setTimeout(() => {
+        window.location.href = url;
+      }, 1500);
+    }
   });
+});
 
-  // Al cargar la página, ocultar loader
-  window.addEventListener("load", () => {
-    ocultarLoader();
-  });
-</script>
+// Ocultar loader al cargar página
+window.addEventListener("load", () => {
+  ocultarLoader();
+});
